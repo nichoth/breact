929a4faba5dee2d66a4337c30b7ca4e09b8335eb
nichoth <nichoth@gmail.com>
2021-08-16 14:54:24 -0700

    more

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ var ReactDOM = require('react-dom')
 
 console.log('ok')
 
-var el = function (prop) {
+var El = function (prop) {
     // return React.createElement('p', {}, ['hello'])
     return <p>hello</p>
 }
 
-ReactDOM.render(React.createElement(el, {}, []),
+ReactDOM.render(<El />,
     document.getElementById('content'))
 
