8893b80a6dc78f541304671aa710d5d8491ae920
nichoth <nichoth@gmail.com>
2021-08-16 14:58:09 -0700

    this one doesnt work!

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-var React = require('react')
+import React, { useState } from 'react';
 var ReactDOM = require('react-dom')
 
 console.log('ok')
 
-var El = function (prop) {
-    // return React.createElement('p', {}, ['hello'])
+var El = function (props) {
     return <p>hello</p>
 }
 
 ReactDOM.render(<El />,
     document.getElementById('content'))
-
