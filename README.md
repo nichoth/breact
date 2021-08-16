413f60912c3361fd5bb1bdc09c03e1438155714b
nichoth <nichoth@gmail.com>
2021-08-16 15:02:27 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# breact
+
+Trying react + browserify
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+var { useState } = require('react');
+var React = require('react')
 var ReactDOM = require('react-dom')
 
 console.log('ok')
 
 var El = function (props) {
+    var [path, setPath] = useState('')
+    console.log('path', path)
     return <p>hello</p>
 }
 
